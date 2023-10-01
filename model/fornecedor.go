54f477b21d7464086848158e3bfd2d6c56334d81
Endy José Alexandre Melo dos Santos <endy.jams@Endys-MBP.local>
2023-10-01 15:52:19 -0300

    changes: adding Fornecedor entity

--- model/fornecedor.go
+++ model/fornecedor.go
@@ -1,1 +1,8 @@
 package model
+
+// Fornecedor representa o fornecedor referente ao estoque de frutas
+// @swaggertype "object"
+type Fornecedor struct {
+	nome     string `json:"nome"`
+	telefone string `json:"telefone"`
+}

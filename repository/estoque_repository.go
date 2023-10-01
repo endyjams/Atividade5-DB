1f49cb876c032fb142e0006fc3413dcb936f93ed
Endy José Alexandre Melo dos Santos <endy.jams@Endys-MBP.local>
2023-10-01 17:18:10 -0300

    changes: adding GetEstoque query method to estoque repository

--- repository/estoque_repository.go
+++ repository/estoque_repository.go
@@ -14,10 +14,28 @@ type FrutaDatabase struct {
 	Db *database.Database
 }
 
-func (frutaDatabase *FrutaDatabase) GetEstoque(nomeFruta string) (*model.Estoque, error) {
+func (frutaDatabase *FrutaDatabase) GetEstoque(nomeFruta string, nomeFornecedor string) (*model.Estoque, error) {
 	var estoque model.Estoque
 
-	err := frutaDatabase.Db.Conn.QueryRow("SELECT * FROM users WHERE cpf=$1", nomeFruta).Scan(&estoque.nomeFruta, &estoque.nomeFornecedor, &estoque.quantidade)
+	err := frutaDatabase.Db.Conn.QueryRow("
+	WITH
+	id_fru AS (
+		SELECT id_fruta 
+		FROM fruta
+		WHERE fruta.nome = $1
+	),
+	id_forn AS (
+		SELECT id_fornecedor 
+		FROM fornecedor
+		WHERE fornecedor.nome = $2
+	)
+	
+	SELECT fruta.nome, fornecedor.nome, estoque.quantidade
+	FROM fornecedor
+	JOIN id_forn ON fornecedor.id_fornecedor = id_forn.id_fornecedor
+	JOIN estoque ON fornecedor.id_fornecedor = estoque.id_fornecedor
+	JOIN id_fru ON estoque.id_fruta = id_fru.id_fruta
+	JOIN fruta ON estoque.id_fruta = fruta.id_fruta;", nomeFruta, nomeFornecedor).Scan(&estoque.nomeFruta, &estoque.nomeFornecedor, &estoque.quantidade)
 
 	if err != nil {
 		return nil, err
